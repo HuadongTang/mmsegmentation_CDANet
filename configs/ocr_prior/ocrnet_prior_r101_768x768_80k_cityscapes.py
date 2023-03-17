72203a8971ec2e412f7424bc6e554c3dc3604a3a
THD <852077607@qq.com>
2023-03-17 12:30:24 +1100

    Update and rename ocrnet_prior_r101_769x769_80k_cityscapes.py to ocrnet_prior_r101_768x768_80k_cityscapes.py

--- configs/ocr_prior/ocrnet_prior_r101_768x768_80k_cityscapes.py
+++ configs/ocr_prior/ocrnet_prior_r101_768x768_80k_cityscapes.py
@@ -1,5 +1,5 @@
 _base_ = [
-    '../_base_/models/ocrnet_r50-d8.py', '../_base_/datasets/cityscapes_769x769.py',
+    '../_base_/models/ocrnet_r50-d8.py', '../_base_/datasets/cityscapes_768x768.py',
     '../_base_/default_runtime.py', '../_base_/schedules/schedule_80k.py'
 ]
 find_unused_parameters = True
